7a4411ef3110e1a92e52f5e014be3bb15294346d
ahfdkun <ahfdkun@163.com>
2017-09-15 02:11:33 +0800

    Spring Boot 入门

--- src/main/java/com/ahfdkun/hello/HelloApplication.java
+++ src/main/java/com/ahfdkun/hello/HelloApplication.java
@@ -3,7 +3,9 @@ package com.ahfdkun.hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import com.ahfdkun.web.HelloController;
+
+@SpringBootApplication(scanBasePackageClasses = HelloController.class)
 public class HelloApplication {
 
 	public static void main(String[] args) {
