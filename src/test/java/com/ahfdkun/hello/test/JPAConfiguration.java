55fbf5fbab84af4140eb3001ce28e52d7efcd97e
ahfdkun <785440131@qq.com>
2018-03-22 17:24:49 +0800

    添加Redis以及Druid

--- src/test/java/com/ahfdkun/hello/test/JPAConfiguration.java
+++ src/test/java/com/ahfdkun/hello/test/JPAConfiguration.java
@@ -1,10 +1,7 @@
 package com.ahfdkun.hello.test;
 
-import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -13,7 +10,6 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
---
+++ src/test/java/com/ahfdkun/hello/test/RedisTest.java
@@ -0,0 +1,60 @@
+package com.ahfdkun.hello.test;
+
+import com.ahfdkun.domain.Department;
+import com.ahfdkun.domain.Role;
+import com.ahfdkun.domain.User;
+import com.ahfdkun.repository.UserRedis;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {RedisConfiguration.class, UserRedis.class})
+public class RedisTest {
+    private static Logger logger = LoggerFactory.getLogger(RedisTest.class);
+
+    @Autowired
+    UserRedis userRedis;
+
+    @Before
+    public void setup(){
+        Department deparment = new Department();
+        deparment.setName("开发部");
+
+        Role role = new Role();
+        role.setName("admin");
+
+        User user = new User();
+        user.setName("user");
+        user.setCreatedate(new Date());
+        user.setDepartment(deparment);
+
+        List<Role> roles = new ArrayList<>();
+        roles.add(role);
+
+        user.setRoles(roles);
+
+        userRedis.delete(this.getClass().getName()+":userByname:"+user.getName());
+        userRedis.add(this.getClass().getName()+":userByname:"+user.getName(), 10L, user);
+
+    }
+
+    @Test
+    public void get(){
+        User user = userRedis.get(this.getClass().getName() + ":userByname:user");
+        Assert.notNull(user);
+        logger.info("======user====== name:{}, deparment:{}, role:{}",
+                user.getName(), user.getDepartment().getName(), user.getRoles().get(0).getName());
+    }
+}
